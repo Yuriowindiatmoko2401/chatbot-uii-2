1be3ef56da84010b15a1878652b0be2cdb521695
yuriowindiatmoko <yurio.windiatmoko@efishery.com>
2022-04-04 02:31:55 +0700

    chore(pre_utils): tidying up w sample

--- pre_utils/pdf_csv.py
+++ pre_utils/pdf_csv.py
@@ -1,7 +1,12 @@
 # usage, python pdf_csv.py input output
 import tabula, sys, os
 
-workdir = os.getcwd()
 
-tabula.convert_into(workdir + '/' + str(sys.argv[1]), workdir + '/' + str(sys.argv[2]), output_format="csv", pages='all')
+def main():
+    workdir = os.getcwd()
 
+    tabula.convert_into(workdir + '/' + str(sys.argv[1]), workdir + '/' + str(sys.argv[2]), output_format="csv", pages='all')
+
+
+if __name__ == "__main__":
+    main()
